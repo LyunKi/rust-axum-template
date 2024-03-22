9e4ce81ba23672a6bb51e985736e3fa4c37c9a60
LyunKi <ly1169134156@163.com>
2024-03-22 21:17:20 +0800

    feat: make it runable

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
-use axum::{response::Html, routing::get, Router};
-use std::env;
+use axum::{routing::get, serve, Router};
+use dotenv::dotenv;
+use std::{env, error::Error, net::SocketAddr};
+use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() {
-    dotenv::dotenv().ok();
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok();
     let host = env::var("HOST")?;
     let port = env::var("PORT")?;
-    let address: std::net::SocketAddr = format!("{}:{}", host, port).parse()?;
+    let address: SocketAddr = format!("{}:{}", host, port).parse()?;
 
-    let app = Router::new().route("/", get(handler));
-
-    // run it
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
-    println!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+    let app = Router::new().route("/health-check", get(handler));
+    let listener = TcpListener::bind(address).await?;
+    serve(listener, app).await?;
+    Ok(())
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+async fn handler() -> String {
+    String::from("Hello, world")
 }
